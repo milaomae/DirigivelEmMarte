ce2df04f38e2adf70289eab0f1cef048e8c7cf31
John <joao.varani@gmail.com>
2017-11-04 01:06:12 -0200

    Create class Caminho

---
+++ DirigivelEmMarte/DirigivelEmMarte/Caminho.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirigivelEmMarte
+{
+    class Caminho
+    {
+        private int cidadeAtual, cidadeDistino, tempo;
+        private double preco, distancia;
+
+        public Caminho(int cidadeAtual, int cidadeDistino, int tempo, double preco, double distancia)
+        {
+            this.cidadeAtual = cidadeAtual;
+            this.cidadeDistino = cidadeDistino;
+            this.tempo = tempo;
+            this.preco = preco;
+            this.distancia = distancia;
+        }
+
+        public int CidadeAtual { get => cidadeAtual; set => cidadeAtual = value; }
+        public int CidadeDistino { get => cidadeDistino; set => cidadeDistino = value; }
+        public int Tempo { get => tempo; set => tempo = value; }
+        public double Preco { get => preco; set => preco = value; }
+        public double Distancia { get => distancia; set => distancia = value; }
+    }
+}
