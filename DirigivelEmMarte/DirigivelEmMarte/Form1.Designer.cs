34aea9ddcbfef366b051964bc52cc098ce37b68d
CAMILA OMAE <milaomae@gmail.com>
2017-11-06 21:51:33 -0200

    inicio da implementação do desenho do caminho

--- DirigivelEmMarte/DirigivelEmMarte/Form1.Designer.cs
+++ DirigivelEmMarte/DirigivelEmMarte/Form1.Designer.cs
@@ -28,20 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnTracarCam = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DirigivelEmMarte.Properties.Resources.Mapa_Marte_sem_rotas;
+            this.pictureBox1.Location = new System.Drawing.Point(46, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 380);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnTracarCam
+            // 
+            this.btnTracarCam.Location = new System.Drawing.Point(368, 408);
+            this.btnTracarCam.Name = "btnTracarCam";
+            this.btnTracarCam.Size = new System.Drawing.Size(146, 23);
+            this.btnTracarCam.TabIndex = 1;
+            this.btnTracarCam.Text = "Traçar Caminho";
+            this.btnTracarCam.UseVisualStyleBackColor = true;
+            this.btnTracarCam.Click += new System.EventHandler(this.btnTracarCam_Click);
+            this.btnTracarCam.Paint += new System.Windows.Forms.PaintEventHandler(this.btnTracarCam_Paint);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(687, 372);
+            this.ClientSize = new System.Drawing.Size(911, 460);
+            this.Controls.Add(this.btnTracarCam);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnTracarCam;
     }
 }
 
--- DirigivelEmMarte/DirigivelEmMarte/Form1.cs
+++ DirigivelEmMarte/DirigivelEmMarte/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,54 @@ namespace DirigivelEmMarte
 {
     public partial class Form1 : Form
     {
+        //ListaSimplesP<Cidade> listaCidades = new ListaSimplesP<Cidade>;
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void btnTracarCam_Click(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        public void LerCidades()
+        {
+
+            StreamReader arq = new StreamReader("C:\\Users\\u17393\\Desktop\\DirigivelEmMarte");
+            String linha = "";
+            int codigo = -1;
+            string cidade = "";
+            int cordx, cordy = 0;
+            while (!arq.EndOfStream)
+            {
+                linha = arq.ReadLine();
+                codigo = Convert.ToInt32(linha.Substring(0, 2).Trim());
+                cidade = linha.Substring(2, 17).Trim();
+                cordx =  Convert.ToInt32(linha.Substring(19, 4).Trim());
+                cordy = Convert.ToInt32(linha.Substring(23).Trim());
+
+                //listaCidades.InserirAntesDoInicio(new Cidade(codigo, cidade, cordx, cordy));
+            }
+
+            arq.Close();
+        }
+
+        public void DesenharCaminho(PaintEventArgs e)
+        {
+            Color cor = Color.Red;
+            Pen pen = new Pen(cor);
+
+            Graphics g = e.Graphics;
+            g.DrawLine(pen, new Point(10, 20), new Point(500, 1000)); 
+        }
+
+        private void btnTracarCam_Paint(object sender, PaintEventArgs e)
+        {
+            DesenharCaminho(e);
         }
     }
 }
