f6bf89c0a2fe62f7dfee8cfbe8620eef0b1b7ed9
milaomae <31636124+milaomae@users.noreply.github.com>
2017-11-07 08:53:26 -0200

    Create caminhoPintado.cs

---
+++ DirigivelEmMarte/DirigivelEmMarte/caminhoPintado.cs
@@ -0,0 +1,41 @@
+
+
+public class caminhoPintado : ListaSimples<caminhoPintado> {
+    
+    Color cor;
+    Point p1, p2;
+    
+    public Point P1
+    {
+      get{ return p1; }
+      set{ p1 = value; }
+    }
+    
+    public Point P2
+    {
+      get{ return p2; }
+      set{ p2 = value; }
+    }
+    
+    public Color Cor
+    {
+      get{ return cor; }
+      set{ cor = value; }
+    }
+    
+    //construtor da classe
+    public caminhoPintado(Color cor, Point p1, Point p2)
+    {
+      this.cor = cor;
+      this.p1 = p1;
+      this.p2 = p2;
+    }
+    
+    public DesenharCaminho(Color cor,Graphics g)
+    {
+      
+      Pen pen = new Pen(cor);
+      g.DrawLine(pen, p1, p2);
+    
+    }
+}
